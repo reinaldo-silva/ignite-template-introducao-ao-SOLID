0002e5b5bd3e2530f41a87fc2ad7002d4ee7d550
Reinaldo Silva <reinaldo.silva.jr@outlook.com>
2021-03-21 23:00:41 -0300

    Desafio completo!!!

---
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -0,0 +1,28 @@
+import { User } from "../../model/User";
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+
+interface IRequest {
+  user_id: string;
+}
+
+class ListAllUsersUseCase {
+  constructor(private usersRepository: IUsersRepository) {}
+
+  execute({ user_id }: IRequest): User[] {
+    const userIsAdmin = this.usersRepository.findById(user_id);
+
+    if (!userIsAdmin) {
+      throw new Error("User not found");
+    }
+
+    if (!userIsAdmin.admin) {
+      throw new Error("User does not have permission");
+    }
+
+    const users = this.usersRepository.list();
+
+    return users;
+  }
+}
+
+export { ListAllUsersUseCase };
---
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
@@ -0,0 +1,20 @@
+import { Request, Response } from "express";
+
+import { TurnUserAdminUseCase } from "./TurnUserAdminUseCase";
+
+class TurnUserAdminController {
+  constructor(private turnUserAdminUseCase: TurnUserAdminUseCase) {}
+
+  handle(request: Request, response: Response): Response {
+    try {
+      const { user_id } = request.params;
+      const user = this.turnUserAdminUseCase.execute({ user_id });
+
+      return response.json(user);
+    } catch ({ message }) {
+      return response.status(404).json({ error: message });
+    }
+  }
+}
+
+export { TurnUserAdminController };
